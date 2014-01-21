2ef5cd1619bf34a6d318006ca4ce72a391ea7f2e
ozagy <bc2521@columbia.edu>
2014-01-20 23:16:21 -0600

    add large size image

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,7 @@
 class Photo < ActiveRecord::Base
   belongs_to :album
   
-  has_attached_file :image, :styles => { :thumb => "60x60>", :medium => "300x300>" },
+  has_attached_file :image, :styles => { :thumb => "60x60>", :medium => "300x300>", :large => "1024x1024>" },
                     :url => "/assets/photos/:id/:style/:basename.:extension",
                     :path => ":rails_root/public/assets/photos/:id/:style/:basename.:extension"
 end
